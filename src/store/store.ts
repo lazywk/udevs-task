9d8279d05a96c49c220d7464b3751ccf421dd2d2
Doniyor Eshmamatov <edoniyorbekdev@gmail.com>
2024-09-17 23:08:09 +0500

    remove other store

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,22 +4,14 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 
 import theme from './theme'
 
-import dashboard from './dashboard/dashboard'
-import { dashboardApi } from './dashboard/dashboardApi'
 
 import orders from './orders/orders'
 
 export const store = configureStore({
     reducer: {
-        [dashboardApi.reducerPath]: dashboardApi.reducer,
         theme,
-        dashboard,
         orders
-    },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(
-            dashboardApi.middleware,
-        ),
+    }
 })
 
 setupListeners(store.dispatch)
